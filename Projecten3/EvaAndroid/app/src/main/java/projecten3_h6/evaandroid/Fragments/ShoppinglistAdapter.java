007e4f0f9fd3dd819e3358e4d6628d48264407ad
Jens Leirens <jens.leirens@gmail.com>
2017-07-04 18:23:42 +0200

    add shoppinglist feature.

---
+++ Projecten3/EvaAndroid/app/src/main/java/projecten3_h6/evaandroid/Fragments/ShoppinglistAdapter.java
@@ -0,0 +1,63 @@
+package projecten3_h6.evaandroid.Fragments;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import projecten3_h6.evaandroid.R;
+
+/**
+ * Created by Mafken on 4/07/2017.
+ */
+
+public class ShoppinglistAdapter extends RecyclerView.Adapter<ShoppinglistAdapter.ShoppinglistViewHolder>{
+
+    private int itemCount;
+    private ArrayList<String> shoppingList;
+
+    public ShoppinglistAdapter(ArrayList<String> shoppingList) {
+        this.shoppingList = shoppingList;
+        this.itemCount = shoppingList.size();
+    }
+
+    @Override
+    public ShoppinglistAdapter.ShoppinglistViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.card_shoppinglist,parent,false);
+        return new ShoppinglistViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(ShoppinglistAdapter.ShoppinglistViewHolder holder, int position) {
+        CheckBox shoppingslistItemName = holder.shoppinglistItemName;
+        //TextView shoppinglistItemCount = holder.shoppinglistItemCount;
+
+        shoppingslistItemName.setText(shoppingList.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return itemCount;
+    }
+
+    public static class ShoppinglistViewHolder extends RecyclerView.ViewHolder {
+
+        @BindView(R.id.shoppinglistItemNamecbx)
+        public CheckBox shoppinglistItemName;
+
+        //@BindView(R.id.shoppinglistItemCount)
+        //public TextView shoppinglistItemCount;
+
+        public ShoppinglistViewHolder(View itemView) {
+            super(itemView);
+            ButterKnife.bind(this,itemView);
+        }
+    }
+}
