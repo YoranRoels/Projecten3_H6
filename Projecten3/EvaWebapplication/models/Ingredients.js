c6832207a0165802ddb1de260c998e72fa2785ef
JoritVergalle <jorit.vergalle@hotmail.com>
2017-07-16 15:40:01 +0200

    Route added to post ingredients to a dish gitignore added to /webapplication for heroku

--- Projecten3/EvaWebapplication/models/Ingredients.js
+++ Projecten3/EvaWebapplication/models/Ingredients.js
@@ -3,7 +3,7 @@ var mongoose = require('mongoose');
 var IngredientSchema = new mongoose.Schema({
     name: String,
     amount: String,
-    dish: { type: mongoose.Schema.Types.ObjectId, ref: 'Dish' }
+    //dish: { type: mongoose.Schema.Types.ObjectId, ref: 'Dish' }
 });
 
 mongoose.model('Ingredient', IngredientSchema);
