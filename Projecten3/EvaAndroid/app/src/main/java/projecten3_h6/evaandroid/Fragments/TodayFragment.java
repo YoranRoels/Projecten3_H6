e629d6b0b114ddd4debaa1a9bd1652b5495d56a7
Mafken <jens.leirens@gmail.com>
2017-07-18 23:22:12 +0200

    add base backend code

--- Projecten3/EvaAndroid/app/src/main/java/projecten3_h6/evaandroid/Fragments/TodayFragment.java
+++ Projecten3/EvaAndroid/app/src/main/java/projecten3_h6/evaandroid/Fragments/TodayFragment.java
@@ -1,14 +1,17 @@
 package projecten3_h6.evaandroid.Fragments;
 
+import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -18,6 +21,7 @@ import java.util.Locale;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import projecten3_h6.evaandroid.Domain.Achievement;
 import projecten3_h6.evaandroid.Domain.CookingTime;
 import projecten3_h6.evaandroid.Domain.Day;
 import projecten3_h6.evaandroid.Domain.Dish;
@@ -25,7 +29,12 @@ import projecten3_h6.evaandroid.Domain.EvaApplication;
 import projecten3_h6.evaandroid.Domain.Ingredient;
 import projecten3_h6.evaandroid.Domain.DishType;
 import projecten3_h6.evaandroid.Domain.User;
+import projecten3_h6.evaandroid.Network.Calls;
+import projecten3_h6.evaandroid.Network.Config;
 import projecten3_h6.evaandroid.R;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 /**
  * Created by jensleirens on 05/07/2017.
@@ -42,12 +51,41 @@ public class TodayFragment extends Fragment {
     @BindView(R.id.todayDishName)TextView dishName;
     private Dish dish;
     User user;
+    List<Dish> dishes = new ArrayList<>();
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_today, container, false);
         ButterKnife.bind(this,v);
+
+        //todo delete test code
+        //test
+        Calls caller = Config.getRetrofit().create(Calls.class);
+        Call<List<Dish>> call = caller.getDishes();
+        call.enqueue(new Callback<List<Dish>>() {
+            @Override
+            public void onResponse(Call<List<Dish>> call, Response<List<Dish>> response) {
+                System.out.println("api call" + response.body());
+                dishes = response.body();
+                Log.e("APICALL", " call successful");
+                Toast.makeText(getActivity().getApplicationContext(),"call successful",Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onFailure(Call<List<Dish>> call, Throwable t) {
+                Log.e("APICALL", "failed to call "+ t.getMessage());
+                //Toast.makeText(getActivity().getApplicationContext(),"geen internet verbinding!",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        System.out.println(dishes);
+
+
+
+        //test
+
+
         // Get User
         Context context = getContext();
         EvaApplication app = (EvaApplication)context.getApplicationContext();
