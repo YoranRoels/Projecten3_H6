eaad2cb5913f3846db48aa8c66352dbf1dabca35
JoritVergalle <jorit.vergalle@hotmail.com>
2017-07-30 15:14:15 +0200

    added model for challenges added routes to get all challenges, get 1, post 1

---
+++ Projecten3/EvaWebapplication/models/Challenges.js
@@ -0,0 +1,17 @@
+var mongoose = require('mongoose');
+
+var ChallengeSchema = new mongoose.Schema({
+    name: String,
+    description: String,
+    challengeType: {
+        type: String,
+        enum:[
+            'EASY',
+            'MEDIUM',
+            'HARD'],
+    },
+});
+
+mongoose.model('Challenge', ChallengeSchema);
+
+
