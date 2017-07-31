cec546b00f7486a1e291dd74792d0d2efdfa67d7
robin van Lierde <robin2383@hotmail.com>
2017-07-31 20:41:17 +0200

    you can now add achievemets + layout change in dishes. Adding ingredients is WIP

--- Projecten3/EvaWebapplication/public/javascripts/AngularApp.js
+++ Projecten3/EvaWebapplication/public/javascripts/AngularApp.js
@@ -9,7 +9,12 @@ app.config([
             .state('home', {
                 url: '/home',
                 templateUrl: 'partials/home',
-                controller: 'MainCtrl'
+                controller: 'MainCtrl',
+                resolve: {
+					postPromise: ['data', function(data) {
+						return data.getAllDishes();
+					}]
+				}
             })
             .state('dishes', {
                 url: '/dishes',
@@ -62,6 +67,11 @@ app.factory('data', ['$http', function($http){
             o.dishes.push(data);
         })
     }
+     o.createAchievement = function(achievement){
+        return $http.post("/achievements",achievement).success(function(data){
+            o.achievements.push(data);
+        })
+    }
     o.getDish = function(id){
         return $http.get('/dishes'+ id).then(function(res) {
 			return res.data;
@@ -79,9 +89,9 @@ app.factory('data', ['$http', function($http){
 }]);
 
 app.controller('MainCtrl', [
-    '$scope',
-    function($scope){
-        $scope.test = 'Hello world!';
+    '$scope','data',
+    function($scope,dishes){
+        $scope.dishes = dishes.dishes;
         
     }]);
 
@@ -89,20 +99,42 @@ app.controller('DishesCtrl', [
     '$scope','data',
     function($scope, dishes){
         $scope.dishes = dishes.dishes;
+         $scope.items = [];
+
+  $scope.itemsToAdd = [{
+    name: '',
+    lastName: ''
+  }];
+
+  $scope.add = function(itemToAdd) {
+
+    var index = $scope.itemsToAdd.indexOf(itemToAdd);
+
+    $scope.itemsToAdd.splice(index, 1);
+
+    $scope.items.push(angular.copy(itemToAdd))
+  }
+
+  $scope.addNew = function() {
+
+    $scope.itemsToAdd.push({
+      firstName: '',
+      lastName: ''
+    })
+  }
         
-        $scope.createDish = function(){
+        
+          $scope.createDish = function(){
             dishes.createDish({
                 name:$scope.name,
                 cookingTime : $scope.cookingTime,
                 difficulty : $scope.difficulty,
                 dishType : $scope.dishType,
-                preparation : $scope.preparation
+                preparation : $scope.preparation,
+                ingredients : $scope.ingredientList
                   
             })
-           
-            
-            
-            
+        
         }
        
         
@@ -112,6 +144,16 @@ app.controller('AchievementsCtrl', [
     '$scope','data',
     function($scope,achievements){
         $scope.achievements= achievements.achievements;
+        
+        $scope.createAchievement=function(){
+            achievements.createAchievement({
+                title: $scope.title,
+                description : $scope.description,
+                achievementType : $scope.achievementType
+                    
+            })
+        }
+        
     }]);
 
 app.controller('NavCtrl', [
