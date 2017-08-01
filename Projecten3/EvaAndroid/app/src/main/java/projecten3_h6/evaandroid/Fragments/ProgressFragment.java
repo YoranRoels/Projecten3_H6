aa7adfc779f2ebea7a526cc9fa80bf2b86d828c7
YoranRoels <yoranroels@gmail.com>
2017-08-01 23:03:55 +0200

    Remove SOUTs.

--- Projecten3/EvaAndroid/app/src/main/java/projecten3_h6/evaandroid/Fragments/ProgressFragment.java
+++ Projecten3/EvaAndroid/app/src/main/java/projecten3_h6/evaandroid/Fragments/ProgressFragment.java
@@ -177,7 +177,6 @@ public class ProgressFragment extends Fragment implements ProgressPickerDialog.D
         Calendar cal = Calendar.getInstance();
         cal.getTime();
 
-        System.out.println("LEL");
         for (int i = 0; i < segmentSize; i++) {
             days.add(new Day(cal.get(Calendar.YEAR), cal.get(Calendar.MONTH), cal.get(Calendar.DAY_OF_MONTH), cal.get(Calendar.DAY_OF_WEEK), cal.get(Calendar.DAY_OF_YEAR)));
             initChallenges(days.get(i));
@@ -263,7 +262,6 @@ public class ProgressFragment extends Fragment implements ProgressPickerDialog.D
     }
 
     public void initChallenges(final Day day) {
-        System.out.println("hello");
         Calls caller = Config.getRetrofit().create(Calls.class);
         Call<List<Challenge>> call = caller.getThreeRandomChallenges();
         call.enqueue(new Callback<List<Challenge>>() {
